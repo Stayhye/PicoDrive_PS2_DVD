96351542fe18b650ccd7558b68eead482de75824
Stayhye <165840599+Stayhye@users.noreply.github.com>
2025-03-01 21:15:17 -0600

    Update emu.c

--- platform/common/emu.c
+++ platform/common/emu.c
@@ -243,25 +243,28 @@ static const char *find_bios(int *region, const char *cd_fname)
 		return NULL;
 	}
 }
-/*
+
 static const char *find_msu(const char *cd_fname)
 {
-	int i;
+	//int i;
 
 	// look for MSU.MD or MD+ rom file. XXX another extension list? ugh...
-	static const char *md_exts[] = { "gen", "smd", "md", "32x" };
+	//static const char *md_exts[] = { "gen", "smd", "md", "32x" };
 	char *ext = strrchr(cd_fname, '.');
 	int extpos = ext ? ext-cd_fname : strlen(cd_fname);
 	strcpy(static_buff, cd_fname);
 	static_buff[extpos++] = '.';
+	/*
 	for (i = 0; i < ARRAY_SIZE(md_exts); i++) {
 		strcpy(static_buff+extpos, md_exts[i]);
 		if (access(static_buff, R_OK) == 0) {
 			printf("found MSU rom: %s\n",static_buff);
 			return static_buff;
 		}
-	}
-	return NULL;
+	} */
+	//return NULL;
+
+ 	return static_buff;
 }
 
 /* check if the name begins with BIOS name */
