e3009d7cc939d0421aea6338841d7feadbf6f696
Stayhye <165840599+Stayhye@users.noreply.github.com>
2026-03-30 21:33:23 -0500

    Update print statement from 'Hello' to 'Goodbye'

--- platform/common/emu.c
+++ platform/common/emu.c
@@ -210,22 +210,22 @@ static const char *find_bios(int *region, const char *cd_fname)
 	for (i = 0; i < count; i++)
 	{
 		emu_make_path(static_buff, files[i], sizeof(static_buff) - 4);
-		strcat(static_buff, ".bin");
+		strcat(static_buff, ".BIN");
 		f = fopen(static_buff, "rb");
 		if (f) break;
 
 		static_buff[strlen(static_buff) - 4] = 0;
-		strcat(static_buff, ".zip");
+		strcat(static_buff, ".ZIP");
 		f = fopen(static_buff, "rb");
 		if (f) break;
 
 		strcpy(static_buff, files[i]);
-		strcat(static_buff, ".bin");
+		strcat(static_buff, ".BIN");
 		f = fopen(static_buff, "rb");
 		if (f) break;
 
 		static_buff[strlen(static_buff) - 4] = 0;
-		strcat(static_buff, ".zip");
+		strcat(static_buff, ".ZIP");
 		f = fopen(static_buff, "rb");
 		if (f) break;
 	}
